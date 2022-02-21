a9b30776ad7d4f2588cb3412272cc74b817ce8af
unknown <you@example.com>
2022-02-21 23:36:20 +0100

    Change debug

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#6@)=)@6=j1cnf$l4^*#f7$4bq5ep!!x4&@rv(uzx=rp#0u73$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'my-shoppit.herokuapp.com']
 
